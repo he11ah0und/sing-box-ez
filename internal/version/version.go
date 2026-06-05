44added379575bdebe98aeca629aa4a57848f07a
he11ah0und <denisshinkevih@gmail.com>
2026-06-05 15:45:23 +0800

    feat: include git commit id in build info
    - Add Commit variable to internal/version/version.go
    - Add BUILD_COMMIT to Makefile via git rev-parse --short HEAD
    - Update Info() to display commit alongside version and date

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,11 +5,12 @@ import "fmt"
 var (
 	Version   = "dev"
 	BuildDate = "unknown"
+	Commit    = "unknown"
 )
 
 func Info() string {
 	if BuildDate == "unknown" || BuildDate == "" {
 		return Version
 	}
-	return fmt.Sprintf("%s (%s)", Version, BuildDate)
+	return fmt.Sprintf("%s (%s %s)", Version, BuildDate, Commit)
 }
